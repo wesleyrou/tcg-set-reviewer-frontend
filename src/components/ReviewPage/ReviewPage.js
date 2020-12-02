aa3979f53dc0b1eeedb0a6cddd16c865a32264c7
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-02 12:52:46 -0600

    work on review page

--- src/components/ReviewPage/ReviewPage.js
+++ src/components/ReviewPage/ReviewPage.js
@@ -2,21 +2,35 @@ import React from 'react';
 
 class ReviewPage extends React.Component {
 
+  componentDidMount() {
+    // check if player has reviewed set before
+    // false = save new review to db
+    // true = this is an old review
+    //    pull ratings
+  }
+
+  // TODO:
+  // - create form submit function
+  // - check db if player has reviewed set before
+
   render() {
     const { currentSet, currentCards } = this.props;
     console.log(currentSet, currentCards);
-
     const cards = currentCards.map(card =>
-      <>
+      <div>
         <img src={card.image_url} alt={card.card_name} />
-        <p>1/5</p>
-      </>
+        <input type="text" />
+      </div>
     );
 
     return (
       <>
         <h2>{currentSet.set_name}</h2>
-        {cards[0]}
+        <form>
+          <button type="submit">Save</button>
+          {cards}
+        </form>
+
       </>
     );
   }
